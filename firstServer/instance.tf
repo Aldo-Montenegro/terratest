3cb01060a69bc576fd152fa65a50fe762a309b96
Aldo-Montenegro <101589767+Aldo-Montenegro@users.noreply.github.com>
2022-03-16 12:32:39 -0600

    Update instance.tf

--- firstServer/instance.tf
+++ firstServer/instance.tf
@@ -1,20 +1,12 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
-  }
-}
-
 provider "aws" {
-region = "us-east-2"
+    region = "us-east-2"
 }
 
-resource "aws_instance" "hello-instance" {
-  ami = "ami-0fb653ca2d3203ac1"
-  instance_type = "t2.micro"
-  tags = {
-    Name = "hello-instance"
-  }
+resource "aws_instance" "instance-terraformcloud" {
+    ami = "ami-0fb653ca2d3203ac1"
+    instance_type = "t2.micro"
+    tags = {
+        Name = "terraformcloud"
+    }
 }
+
