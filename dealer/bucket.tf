ccecc1a40803bab279f378758d1dd6f2670b51c4
Aldo Montenegro <aldo.mongue@gmail.com>
2022-03-16 14:19:40 -0600

    Ejercicio bucket s3 update

--- dealer/bucket.tf
+++ dealer/bucket.tf
@@ -7,7 +7,7 @@ variable "content" {}
 provider "aws" {
     region = var.region
     access_key = var.AWS_ACCESS_KEY_ID
-    secret_key = var.AWS_ACCESS_KEY_ID
+    secret_key = var.AWS_SECRET_KEY_ID
 }
 
 /*------------bucket------------*/
