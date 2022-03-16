c69c1453fcccf455763de53030ee2ed60a0e1b87
Aldo Montenegro <aldo.mongue@gmail.com>
2022-03-16 14:51:53 -0600

    correción bucket

--- dealer/bucket.tf
+++ dealer/bucket.tf
@@ -1,28 +1,33 @@
-variable "AWS_ACCESS_KEY_ID" {}
-variable "AWS_SECRET_KEY_ID" {}
-
-provider "aws" {
-    region = var.region
-    access_key = var.AWS_ACCESS_KEY_ID
-    secret_key = var.AWS_SECRET_KEY_ID
-}
-
-/*------------bucket------------*/
-
-resource "aws_s3_bucket" "bucket1" {
-  bucket = var.bucket
-  acl = "public-read"
-  website {
-      index_document = "index.html"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
   }
 }
 
-/*------------content------------*/
+variable "AWS_ACCESS_KEY_ID" {}
+variable "AWS_SECRET_ACCESS_KEY" {}
+
+provider "aws" {
+  region = var.region
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
+}
+
+resource "aws_s3_bucket" "bucket1" {
+  bucket = var.bucket
+  acl    = "public-read"
+  website {
+    index_document = "index.html"
+  }
+}
 
 resource "aws_s3_bucket_object" "object1" {
-  bucket = aws_s3_bucket.bucket1.bucket
-  key    = "index.html"
-  acl = "public-read"
-  content = var.content
+  bucket       = aws_s3_bucket.bucket1.bucket
+  key          = "index.html"
+  acl          = "public-read"
+  content      = var.content
   content_type = "text/html"
 }
