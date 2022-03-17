df19a41529de8029ab19fa5d0b42eced1f5c5ccc
Aldo Montenegro <aldo.mongue@gmail.com>
2022-03-17 10:45:35 -0600

    terraform CLI

---
+++ InstanceCLI/main.tf
@@ -0,0 +1,32 @@
+terraform {
+  cloud {
+    organization = "HSBCTerra"
+
+    workspaces {
+      name = "InstanceCLI"
+    }
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+variable "AWS_ACCESS_KEY_ID" {}
+variable "AWS_SECRET_ACCESS_KEY" {}
+
+provider "aws"{ 
+  region = "us-east-2"
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
+}
+
+resource "aws_instance" "instanceCLI" {
+  ami = "ami-0fb653ca2d3203ac1"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "instanceCLI"
+  }
+}
